435406a1c8a30832a187f008bd77284501438a25
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2023-11-25 19:58:15 +0530

    latest bilal

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import NavigationBar from '../components/NavigationBar1'
+import NavigationBar1 from '../components/NavigationBar1'
 import Footer from '../components/Footer';
 import '../styles/faq.css'
 
 export default function Faq() {
   return (
     <div>
-        <NavigationBar/>
+        <NavigationBar1/>
         <div className='newFaqs'>
         </div>
         
