2ca1995ef5679c94400eceb8a6116203be2a1baf
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2023-11-26 02:05:33 +0530

    create accounts done, store image in sellerView1/2

--- src/pages/CreateSeller.js
+++ src/pages/CreateSeller.js
@@ -6,13 +6,14 @@ import '../styles/accountcreation.css'
 
 export default function AccountCreation() {
 
+
+    //same functions as in CreateCustomer
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [mobileNumber, setMobileNumber] = useState('');
     const [address, setAddress] = useState('');
     const [businessName, setBusinessName] = useState('');
 
-    const [id, setId] = useState('')
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -28,15 +29,14 @@ export default function AccountCreation() {
             businessName: businessName
         })
         .then(response =>{
-        if(response.data === true){
-            setId(response.data[0])
+        if(response.data){
             axios.post('http://localhost:8083/seller/recordCredentials',{
-            id: id,
+            id: response.data.id,
             password: password,
             username: username,
             })
             .then(response2 =>{
-            if(response2.data === id){
+            if(response2.data === response.data.id){
                 navigate("/product")
             }
             else{
@@ -54,24 +54,22 @@ export default function AccountCreation() {
       <div>
         <Header />
         <div className='userDetailsForm'>
-
-            <form action="">
-
+            <form onSubmit={handleLogin}>
                 <div className='formLeft'>
 
                     <label><b>Create account for seller</b></label><br/><br/>
 
                     <label htmlFor="txtFullName">Your name</label><br/>
-                    <input type="text" id="txtFullName" name='name' autoComplete='off'/><br/>
+                    <input type="text" id="txtFullName" name='name' autoComplete='off' onChange={(e) => setName(e.target.value)} required/><br/>
 
                     <label htmlFor="txtEmail">Email</label><br/>
-                    <input type="email" id="txtEmail" name='email'autoComplete='off'/><br/>
+                    <input type="email" id="txtEmail" name='email'autoComplete='off' onChange={(e) => setEmail(e.target.value)} required/><br/>
 
                     <label htmlFor="txtUsername">Username</label><br/>
-                    <input type="text" id="txtUsername" name='username' autoComplete='off'/><br/>
+                    <input type="text" id="txtUsername" name='username' autoComplete='off' onChange={(e) => setUsername(e.target.value)} required/><br/>
 
                     <label htmlFor="txtPassword">Passwrod</label><br/>
-                    <input type="password" id="txtPassword" name='password'/><br/>
+                    <input type="password" id="txtPassword" name='password' onChange={(e) => setPassword(e.target.value)} required/><br/>
 
                     <label htmlFor="">Re-enter password</label><br/>
                     <input type="password" id="" /><br/>
@@ -83,11 +81,11 @@ export default function AccountCreation() {
                 <div className='formRight'>
                     <br/><br/>
                     <label htmlFor="mobile">Mobile Number</label><br/>
-                    <input type="text" id="mobile" autoComplete='off'/><br/>
+                    <input type="text" id="mobile" autoComplete='off' onChange={(e) => setMobileNumber(e.target.value)} required/><br/>
                     <label htmlFor="txtAddress">Address</label><br/>
-                    <textarea id="txtAddress" rows="5" autoComplete='off'></textarea><br/>
+                    <textarea id="txtAddress" rows="5" autoComplete='off' onChange={(e) => setAddress(e.target.value)} required/><br/>
                     <label htmlFor="txtBusinessName">Business Name</label><br/>
-                    <input type="text" id="txtBusinessName" autoComplete='off'/><br/>
+                    <input type="text" id="txtBusinessName" autoComplete='off' onChange={(e) => setBusinessName(e.target.value)} required/><br/>
                     
                 </div>
             </form>
